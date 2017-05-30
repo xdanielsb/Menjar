3d073df8e376bae323c7ffc83f50ebee7198c584
xdanielsb <dfsantosbu@unal.edu.co>
2017-05-30 09:04:39 -0500

    Add functionality to locate the lat and lngs in the map after insert a product.

--- menjar/app/assets/javascripts/google_maps.js
+++ menjar/app/assets/javascripts/google_maps.js
@@ -1,7 +1,7 @@
 var bogota = {lat: 4.710989, lng: -74.072092};
 var map;
 var marker;
-function initMap() {
+function initMap2() {
 
     map = new google.maps.Map(document.getElementById('google_map'), {
       zoom: 15,
