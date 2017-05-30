6e67fcb1d5d0ecda1c35dc3da25be023a573c0f8
xdanielsb <dfsantosbu@unal.edu.co>
2017-05-29 20:35:21 -0500

    Config views and root page, change to rails standard.

--- menjar/config/routes.rb
+++ menjar/config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   resources :consumers
   resources :providers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "welcome#index"
 end
